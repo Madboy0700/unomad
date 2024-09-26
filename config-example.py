018f15ecaedb45a422cd9c7e2fcd6a2df7da6b05
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-09-26 16:56:52 +0300

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -6,11 +6,12 @@ player_game = {}
 timeout = 120
 minimum_players = 2
 
-sudoers = [123456789]
+sudoers = [7242000752]
 
-API_ID = ""
-API_HASH = ""
+API_ID = "3939406"
+API_HASH = "e11d0eaec136b1047974ab098041e9f2"
 
 # --- Telegram config ---
+BOT_TOKEN = "7504787456:AAFzHc2CcmFIUTla6AIjdJAu2nJZCkfIUHI"
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, plugins={"root": "unu.plugins"})
