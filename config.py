6dd772520ec4d25f27d5aa03827d480758d1b321
Madboy0700 <bobimobi07@gmail.com>
2024-10-12 14:37:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ player_game = {}
 timeout = 120
 minimum_players = 2
 
-sudoers = [7242000752]
+sudoers = [5901320319]
 
-API_ID = "3939406"
-API_HASH = "e11d0eaec136b1047974ab098041e9f2"
+API_ID = "28121536"
+API_HASH = "57d552d05f2a76244291d9eb330294c2"
 
 # --- Telegram config ---
-BOT_TOKEN = "7504787456:AAFzHc2CcmFIUTla6AIjdJAu2nJZCkfIUHI"
+BOT_TOKEN = "8030899275:AAGZQlpJqth6cc37FWrustudh5OGyBftA64"
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, plugins={"root": "unu.plugins"})
